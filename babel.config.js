cf52e9fcc64afdde0cf93f47cc46ca4e0761a2ce
erfajc97 <erfacrypto@gmail.com>
2024-10-24 15:11:08 -0300

    [ADD] test

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-// babel.config.js
-module.exports = {
-  presets: [
-    "@babel/preset-env", // Transforma el código moderno de JavaScript
-    "@babel/preset-react", // Transforma JSX
-    "@babel/preset-typescript", // Transforma TypeScript
-  ],
-};
